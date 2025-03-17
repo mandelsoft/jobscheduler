f5c7ae55a5c71d6ff2285e77a5caa4cb7af31f7e
Uwe Krueger <uwe.krueger@goldmail.de>
2025-03-17 12:31:03 +0100

    migrate textspinner

--- examples/progress/textspinner/main.go
+++ examples/progress/textspinner/main.go
@@ -6,23 +6,27 @@ import (
 	"os"
 	"time"
 
-	"github.com/mandelsoft/jobscheduler/uiprogress"
+	"github.com/mandelsoft/jobscheduler/ttyprogress"
 )
 
 func main() {
-	p := uiprogress.New(os.Stdout)
+	p := ttyprogress.New(os.Stdout)
 
 	for s := 0; s < 3; s++ {
-		bar := uiprogress.NewTextSpinner(p, 5, 3).
-			PrependFunc(uiprogress.Message(fmt.Sprintf("working on task %d...", s+1))).
-			AppendElapsed()
+		bar, _ := ttyprogress.NewTextSpinner().
+			SetPredefined(5).
+			SetView(3).
+			SetFollowUpGap("> ").
+			PrependFunc(ttyprogress.Message(fmt.Sprintf("working on task %d...", s+1))).
+			AppendElapsed().
+			Add(p)
 
 		go func() {
 			// starts automatically, with the first write
 			steps := 6 + rand.Int()%10
 			for i := 0; i <= steps; i++ {
 				t := 500 + 200*(rand.Int()%6)
-				fmt.Fprintf(bar, "doing step %d[%dms]\n", i, t)
+				fmt.Fprintf(bar, "doing step %d\n", i)
 				time.Sleep(time.Duration(t) * time.Millisecond)
 			}
 			bar.Close()
